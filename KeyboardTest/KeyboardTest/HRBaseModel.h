deac4ad77821650559ba7a793806293059bd1c0c
henry <henryzhangios@gmail.com>
2015-09-22 16:23:54 +0800

    添加数据基类model

---
+++ KeyboardTest/KeyboardTest/HRBaseModel.h
@@ -0,0 +1,15 @@
+//
+//  HRBaseModel.h
+//  KeyboardTest
+//
+//  Created by ZhangHeng on 15/9/22.
+//  Copyright © 2015年 ZhangHeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HRBaseModel : NSObject
+
+-(id)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
---
+++ KeyboardTest/KeyboardTest/HRBaseModel.m
@@ -0,0 +1,64 @@
+//
+//  HRBaseModel.m
+//  KeyboardTest
+//
+//  Created by ZhangHeng on 15/9/22.
+//  Copyright © 2015年 ZhangHeng. All rights reserved.
+//
+
+#import "HRBaseModel.h"
+
+@implementation HRBaseModel
+
+-(id)initWithDictionary:(NSDictionary *)dictionary{
+    self = [super init];
+    if(self){
+        for(NSString *key in [dictionary allKeys]){
+            id obj = [dictionary objectForKey:key];
+            if(!obj || [obj isKindOfClass:[NSNull class]]){
+                NSLog(@"%@ is null",key);
+                obj = @"";
+            }
+            if([obj isKindOfClass:[NSDictionary class]]){
+                [self configDicValue:obj withKey:key];
+            }else if([obj isKindOfClass:[NSArray class]]){
+                [self configArrayValue:obj withKey:key];
+            }else{
+                [self setValue:obj forKeyPath:key];
+            }
+        }
+    }
+    return self;
+}
+
+//简单的只有一层的数据对象型属性
+-(void)configDicValue:(id)obj withKey:(NSString *)key{
+    Class clazz = NSClassFromString(key);
+    if(clazz){
+        id myObj = [[clazz alloc] initWithDictionary:obj];
+        [self setValue:myObj forKeyPath:key];
+    }else{
+        NSLog(@"%@ undeclare",key);
+    }
+}
+
+//简单的数组类型
+-(void)configArrayValue:(id)obj withKey:(NSString *)key{
+    Class clazz = NSClassFromString(key);
+    if(!clazz){
+        NSLog(@"%@ undeclare",key);
+    }else{
+        NSMutableArray *arrayObj = [NSMutableArray new];
+        for(NSDictionary *dic in obj){
+            id item = [[clazz alloc] initWithDictionary:dic];
+            [arrayObj addObject:item];
+        }
+        [self setValue:arrayObj forKeyPath:key];
+    }
+}
+
+-(void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    NSLog(@"key:%@ not exist",key);
+}
+
+@end
