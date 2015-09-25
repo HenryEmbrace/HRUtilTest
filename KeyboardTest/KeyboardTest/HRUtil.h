be3f805cae34ab2e5bd2aac96810d81000266ab4
henry <henryzhangios@gmail.com>
2015-09-25 13:26:39 +0800

    新增颜色取值方法

--- KeyboardTest/KeyboardTest/HRUtil.h
+++ KeyboardTest/KeyboardTest/HRUtil.h
@@ -9,10 +9,13 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#define UIColorFromRGB(rgbValue,A) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:A]
+
 @interface HRUtil : NSObject
 
 typedef void(^SaveVideoCompleted)(NSString *filePath);
 typedef void(^SaveVideoFailed)(NSError *error);
+
 
 /**
  *  @author Henry
