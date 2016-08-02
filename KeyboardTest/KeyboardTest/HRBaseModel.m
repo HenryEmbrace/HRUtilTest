6d74656e9ec16d5486630c66236746e4de2acee9
henry <henryzhangios@gmail.com>
2016-08-02 17:17:26 +0800

    类型问题修复

--- KeyboardTest/KeyboardTest/HRBaseModel.m
+++ KeyboardTest/KeyboardTest/HRBaseModel.m
@@ -21,14 +21,52 @@
             id obj = [dictionary objectForKey:key];
             if(!obj || [obj isKindOfClass:[NSNull class]]){
                 NSLog(@"%@ is null",key);
-                obj = @"";
             }
             if([obj isKindOfClass:[NSDictionary class]]){
                 [self configDicValue:obj withKey:key];
             }else if([obj isKindOfClass:[NSArray class]]){
                 [self configArrayValue:obj withKey:key];
             }else{
-                [self setValue:obj forKeyPath:key];
+                objc_property_t property = class_getProperty([self class], key.UTF8String);
+                const char * type = property_getName(property);
+                NSString * typeString = [NSString stringWithUTF8String:type];
+                NSArray * attributes = [typeString componentsSeparatedByString:@","];
+                NSString * typeAttribute = [attributes objectAtIndex:0];
+                NSString * propertyType = [typeAttribute substringFromIndex:1];
+                const char * rawPropertyType = [propertyType UTF8String];
+                
+                //根据声明的属性类型处理，一切以声明的类型为准进行转model
+                //如果是数字型的
+                if (strcmp(rawPropertyType, @encode(float)) == 0){
+                    //it's a float
+                    if([obj isKindOfClass:[NSNumber class]]){
+                        [self setValue:obj forKeyPath:key];
+                    }else if([obj isKindOfClass:[NSString class]]){
+                        [self setValue:@([obj intValue]) forKey:key];
+                    }else{
+                        NSLog(@"type error ");
+                    }
+                }else if(strcmp(rawPropertyType, @encode(int)) == 0){
+                    if([obj isKindOfClass:[NSNumber class]]){
+                        [self setValue:obj forKeyPath:key];
+                    }else if([obj isKindOfClass:[NSString class]]){
+                        [self setValue:@([obj floatValue]) forKey:key];
+                    }else{
+                        NSLog(@"type error ");
+                    }
+                }else if (strcmp(rawPropertyType, @encode(id)) == 0) {
+                    //其它类型稍后处理
+                    [self setValue:[NSString stringWithFormat:@"%d",[obj intValue]] forKey:key];
+                } else {
+                    // String型
+                    if([obj isKindOfClass:[NSNumber class]]){
+                        [self setValue:[NSString stringWithFormat:@"%d",[obj intValue]] forKeyPath:key];
+                    }else if([obj isKindOfClass:[NSString class]]){
+                        [self setValue:obj forKey:key];
+                    }else{
+                        NSLog(@"type error ");
+                    }
+                }
             }
         }
     }
